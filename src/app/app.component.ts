9387ac4229f9829f9bea3a916cdea0604f484827
camelsoft-p2 <zkallel81@gmail.com>
2021-12-15 12:17:04 +0100

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+const mediaConstraints = {
+  audio:true,
+  video:{width:720,height:540}
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'videoCall';
+  private UserStream!:MediaStream
+  @ViewChild("userVideoStream")userVideoStream!:ElementRef
+  ngOnInit() {
+
+  }
+  ngAfterViewInit():void{
+    this.requestMediaDevices()
+  }
+  private async requestMediaDevices():Promise<void>{
+   this.UserStream=await navigator.mediaDevices.getUserMedia(mediaConstraints)
+    this.userVideoStream.nativeElement.srcObject=this.UserStream
+  }
 }
